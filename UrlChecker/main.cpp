290ab1a84a77937e01e1262267cbdeff6bfade52
Anton Sapunov <fort.sav.28@gmail.com>
2018-07-20 00:50:42 +0900

    Завершена работа с параметрами запуска

---
+++ UrlChecker/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <fstream>
+
+namespace po = boost::program_options;
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(int argc, char* argv[])
+{
+    int n; // Number of connect-attempts
+    int t; // Delay before next connect
+    string inputFile;
+    vector<string> urls;
+
+    try
+    {
+        // Commandline parse
+        po::options_description desc("Options");
+        desc.add_options()
+                ("help,h", "Display this help and exit")
+                ("input,i", po::value<string>(&inputFile), "File conteining urls")
+                ("number,n", po::value<int>(&n)->default_value(3), "Number of connection attempts")
+                ("time-wait,t", po::value<int>(&t)->default_value(5), "Delau before next connect")
+                ;
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc) , vm);
+        po::notify(vm);
+
+        // Parameter processing
+        if(vm.count("help"))
+        {
+            cout << "Usage: " << argv[0] << " -i FILE -n NUMBER -t DELAY" << endl;
+            cout << desc << endl;
+            return 1;
+        }
+        // Отсутствие обязательного параметра
+        if(!vm.count("input"))
+        {
+            cout<< "There is no input file" << endl;
+            return 1;
+        }
+
+        //Config file parse
+        po::options_description fileDesc("Options in config file");
+        fileDesc.add_options()
+                ("url", po::value<vector<string> >(&urls), "url that be checked")
+                ;
+
+        std::fstream configFile(inputFile);
+        if(!configFile.is_open())
+        {
+            cout << "Can`t open config file" << endl;
+            return 1;
+        }
+        else
+        {
+            po::store(po::parse_config_file(configFile, fileDesc), vm);
+            po::notify(vm);
+        }
+
+    }
+    catch(std::exception &e)
+    {
+        cout << e.what() << endl;
+        return 1;
+    }
+
+    return 0;
+}
